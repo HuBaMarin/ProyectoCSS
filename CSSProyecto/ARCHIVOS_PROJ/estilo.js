e137416c944c8b1d5bb5e17c656c6bbf7bd959d3
HuBaMarin <147373478+HuBaMarin@users.noreply.github.com>
2024-02-15 14:00:54 +0100

    Delete CSSProyecto directory

--- CSSProyecto/ARCHIVOS_PROJ/estilo.js
+++
@@ -1,8 +0,0 @@
-/*se ha añadido este archivo js para marcar la zona del menú en la que me encueentro*/
-let hipervinculos = document.querySelectorAll(".divNav a");
-
-hipervinculos.forEach(element => {
-    if (element.href === document.URL) {
-        element.className = "active";
-    }
-});
